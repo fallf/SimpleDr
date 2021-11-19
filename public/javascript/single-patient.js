5198a0e9ae6dbd18b25b1b07fb0b22d7e00db407
Marvin Ren <marvin.ren@yale.edu>
2021-11-19 17:14:21 -0500

    update patient

--- public/javascript/single-patient.js
+++ public/javascript/single-patient.js
@@ -3,13 +3,16 @@ async function patientCommnetHandler() {
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
+    const p_email = document.querySelector('#pat-email').innerText.trim();
+    
     console.log(p_doc_comment)
       console.log(id)
-    if (p_doc_comment && id) {
-        const response = await fetch('/api/patient/:id', {
+    if (p_doc_comment && id && p_email) {
+        const response = await fetch(`/api/patient/${id}`, {
             method: 'PUT',
             body:JSON.stringify({
-                p_doc_comment
+                p_doc_comment,
+                p_email
             }),
             headers: {'Content-Type':'application/json'}
         });
